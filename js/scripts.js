a575ff423cbd025678e509780aafa376169741b8
Daniel Kuhn <daniel.kuhn.dev@gmail.com>
2024-02-26 07:27:23 +0100

    fix form + change future project text

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,52 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the form and attach a submit event listener
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
+        // Validate each field and update the invalid-feedback div
+        if (!validateName()) {
+            updateInvalidFeedback('name', 'Please enter only letters and spaces.');
+            event.preventDefault(); // Prevent form submission if validation fails
+        }
+        if (!validateEmail()) {
+            updateInvalidFeedback('email', 'Please enter a valid email address.');
+            event.preventDefault();
+        }
+        if (!validatePhone()) {
+            updateInvalidFeedback('phone', 'Please enter a valid phone number (e.g., +49 152 3456 7890).');
+            event.preventDefault();
+        }
+    });
+
+    // Function to validate name
+    function validateFirstName() {
+        var nameInput = document.getElementById('first');
+        return /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u.test(nameInput.value);
+    }
+
+    function validateLastName() {
+        var nameInput = document.getElementById('last');
+        return /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u.test(nameInput.value);
+    }
+
+    // Function to validate email
+    function validateEmail() {
+        var emailInput = document.getElementById('email');
+        return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(emailInput.value);
+    }
+
+    // Function to validate phone
+    function validatePhone() {
+        var phoneInput = document.getElementById('phone');
+        return /^[0-9]{3} [0-9]{3}-[0-9]{4}$/.test(phoneInput.value);
+    }
+
+    // Function to update invalid-feedback div
+    function updateInvalidFeedback(fieldName, message) {
+        var invalidFeedback = document.querySelector('#' + fieldName + ' + .invalid-feedback');
+        invalidFeedback.textContent = message;
+    }
+});
